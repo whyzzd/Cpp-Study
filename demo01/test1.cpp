462bfccdbfb8786fe6e02ffad225636db876cf95
whyzzd <whyzzd@gmail.com>
2021-04-06 23:09:07 +0800

    上传了三个文件

---
+++ demo01/test1.cpp
@@ -0,0 +1,31 @@
+#include"swap.h"
+
+int main()
+{
+	//char arry[4] = { 'a', 'b', 'c', '\0' };
+	////cout << arry[0] << arry[1] << arry[2] << endl;
+	//cout << &arry << endl;
+
+	//char *p = arry;
+	//cout << "&p:" << &p
+	//	<< "   " << p
+	//	<< "   " << arry << endl;
+
+	//cout << arry << arry[0] << endl;
+	//getchar();
+	//return 0;
+
+	char arr[5] = { 'a','b','c' };
+	char *p;
+	int arr1[3] = { 1,3,4 };
+	p = arr;
+	cout << p << endl;
+	p++;
+	cout << p << endl;
+
+
+
+	system("pause");
+	return 0;
+
+}
